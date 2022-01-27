f6c9ad22edd73555b89ee83d191ad59a29df330b
DEEPAK KUMAR VERMA <65648568+deepakkumarvermabca@users.noreply.github.com>
2022-01-27 19:02:41 +0530

    Add files via upload

---
+++ Equilibrium Index.cpp
@@ -0,0 +1,15 @@
+int findEquilibriumIndex(vector<int> &arr) 
+{
+    int sum=0;
+    for(int i=0;i<arr.size();i++)
+        sum += arr[i];
+    int left = 0;
+    for(int i=0;i<arr.size();i++)
+    {
+        sum -= arr[i];
+        if(left==sum)
+            return i;
+        left += arr[i];
+    }
+    return -1;
+}
